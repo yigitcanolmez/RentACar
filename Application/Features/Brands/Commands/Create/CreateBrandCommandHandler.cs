ccfbce858ef14aedc73f03c9795cea586d6ce2f2
yigitcanolmez <yigitcanolmez@outlook.com>
2024-08-16 22:47:51 +0300

    implemented cqrs with mediatr and added basecontroller with singleton pattern.

---
+++ Application/Features/Brands/Commands/Create/CreateBrandCommandHandler.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+
+namespace Application.Features.Brands.Commands.Create;
+
+public class CreateBrandCommandHandler : IRequestHandler<CreateBrandCommand, CreatedBrandResponse>
+{
+    public Task<CreatedBrandResponse> Handle(CreateBrandCommand request, CancellationToken cancellationToken)
+    {
+        var response = new CreatedBrandResponse()
+        {
+            Id = Guid.NewGuid(),
+            Name = request.Name
+        };
+        return Task.FromResult(response);
+    }
+}
--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,9 +1,14 @@
+using Application;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddApplicationServices();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
