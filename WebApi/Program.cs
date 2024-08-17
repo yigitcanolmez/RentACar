9aee3bab4e0794fdfb83642eae585c0c150b0be0
yigitcanolmez <yigitcanolmez@outlook.com>
2024-08-17 20:04:25 +0300

    implemented generic repo pattern

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,11 +1,14 @@
 using Application;
+using Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
 builder.Services.AddApplicationServices();
+builder.Services.AddPersistenceServices(builder.Configuration);
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 
