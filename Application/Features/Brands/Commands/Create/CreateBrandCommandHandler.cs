161c3896797cb25dd93b647d1c11942044721d87
yigitcanolmez <yigitcanolmez@outlook.com>
2024-08-17 21:35:34 +0300

    added automapper with inmemory database

--- Application/Features/Brands/Commands/Create/CreateBrandCommandHandler.cs
+++ Application/Features/Brands/Commands/Create/CreateBrandCommandHandler.cs
@@ -1,16 +1,30 @@
-﻿using MediatR;
+﻿using Application.Services.Repositories;
+using AutoMapper;
+using Domain.Entities;
+using MediatR;
 
 namespace Application.Features.Brands.Commands.Create;
 
 public class CreateBrandCommandHandler : IRequestHandler<CreateBrandCommand, CreatedBrandResponse>
 {
-    public Task<CreatedBrandResponse> Handle(CreateBrandCommand request, CancellationToken cancellationToken)
+    private readonly IBrandRepository _brandRepository;
+    private readonly IMapper _mapper;
+
+    public CreateBrandCommandHandler(IBrandRepository brandRepository, IMapper mapper)
     {
-        var response = new CreatedBrandResponse()
-        {
-            Id = Guid.NewGuid(),
-            Name = request.Name
-        };
-        return Task.FromResult(response);
+        _brandRepository = brandRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<CreatedBrandResponse> Handle(CreateBrandCommand request, CancellationToken cancellationToken)
+    {
+        var brand = _mapper.Map<Brand>(request);
+        brand.Id = Guid.NewGuid();
+
+        await _brandRepository.AddAsync(brand);
+
+        var response = _mapper.Map<CreatedBrandResponse>(brand);
+
+        return response;
     }
 }
